e8cac9d91e63ba90f01caf106f0d16026b80a10e
bernkaranja <bernkanja@gmail.com>
2022-09-11 05:15:19 -0700

    re-do of task $

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
  * main-prints
- * 
+ *
  * returns 0 always
  */
-int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
