f3561f382eae027c6bcb2a5d3cedfdd94f136c7c
bernkaranja <bernkanja@gmail.com>
2022-09-11 12:41:58 -0700

    task 5 if variables

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ */
+
 int main(void)
 
 {
+	char n;
 
-char c;
+	for (n = '0'; n <= '9'; n++)
 
-for (c = '0'; c <= '9'; c++)
+	{
+		putchar(n);
 
-{
+	}
 
-	putchar(c);
+	
+	putchar('\n');
 
+	return (0);
 }
-
-for (c = 'a'; c <= 'f'; c++)
-
-{
-
-	putchar(c);
-
-}
-
-putchar('\n');
-
-return (0);
-
-}
-
-
-clear
