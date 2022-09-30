c4d9b7239ff2b4ee5cf2818073f26c1a2a4223a8
bernkaranja <bernkanja@gmail.com>
2022-09-30 14:56:48 -0700

    task 2 arg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,6 +5,7 @@
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -14,9 +15,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 0)
 	{
-		/*WHILE - Print each
-		 *
-		 * arguments*/
+		/*WHILE - Print each arguments*/
 		while (count < argc)
 		{
 			printf("%s\n", argv[count]);
